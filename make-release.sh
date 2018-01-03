8d79508228a79be92abe04f5c44640a3a25696b4
rhysd <lin90162@yahoo.co.jp>
2018-01-03 19:00:31 +0900

    make releases containing version number

--- make-release.sh
+++ make-release.sh
@@ -3,6 +3,7 @@
 set -e
 
 name=github-release-test
+ver="v1.2.3"
 
 rm -rf release
 gox -verbose
@@ -12,10 +13,15 @@ cd release
 for bin in *; do
     if [[ "${bin}" == *windows* ]]; then
         cmd="${name}.exe"
+        mv "$bin" "$cmd"
+        release="${bin//$name/${name}_${ver}}"
+        zip "${release}.zip" "$cmd"
     else
         cmd="$name"
+        mv "$bin" "$cmd"
+        release="${bin//$name/${name}_${ver}}"
+        tar cf "${release}.tar" "$cmd"
+        gzip "${release}.tar"
     fi
-    mv "$bin" "$cmd"
-    xz "${cmd}"
-    mv "${cmd}.xz" "${bin}.xz"
+    rm "$cmd"
 done
